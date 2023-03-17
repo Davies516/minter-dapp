3c8289e184249b165941dba945fa38417f43ee5f
Davies516 <daviesmdaniel@gmail.com>
2023-03-17 17:16:24 +1030

    changed time

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 20; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2023-06-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = 2022-06-08T11:30:48+00:00; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 750; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x4d7b7803696D6bb33B4EAD2cD5BAA2aB8C065026"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
