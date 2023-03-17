e488fc56e77ac3750744994a711416801e063469
Davies516 <daviesmdaniel@gmail.com>
2023-03-17 17:58:28 +1030

    mint button updates

--- frontend/js/abi.js
+++ frontend/js/abi.js
@@ -1,1 +1,6 @@
-const abi = []
+const abi = {
+    "response": "OK",
+    "chain": "goerli",
+    "contract_address": "0xD4F35Ced6Ff95CaC5AaE630b4d8937D3ad3e9c3A",
+    "transaction_hash": "0xf306612789efb216a02de76d54775796f1cdd22a5d7b7adbc7c3a56888e22933"
+  }
